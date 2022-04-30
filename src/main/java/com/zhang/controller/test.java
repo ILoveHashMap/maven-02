9cd81426cec3ae5c618cf1699a57d7d4b0f8cd86
zhangjinghai <1424061454@qq.com>
2022-04-30 21:26:46 +0800

    second commit

--- src/main/java/com/zhang/controller/test.java
+++ src/main/java/com/zhang/controller/test.java
@@ -3,6 +3,7 @@ package com.zhang.controller;
 public class test {
     public static void main(String[] args) {
         System.out.println("我爱github");
+        System.out.println("我爱github2");
 
     }
 }
