050f6ad0fab35dd3a9f57c0250799b54c96c9a97
zhangjinghai <1424061454@qq.com>
2022-04-30 21:33:39 +0800

    hot-fix commit1

--- src/main/java/com/zhang/controller/test.java
+++ src/main/java/com/zhang/controller/test.java
@@ -4,6 +4,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("我爱github");
         System.out.println("我爱github2");
+        System.out.println("我爱github3");
 
     }
 }
